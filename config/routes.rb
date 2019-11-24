3d0c7a3d188f52ff58c3800f082eaed09e813ad3
Elana Ryznar <eryzna@gmail.com>
2019-11-24 03:01:26 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   @post.update(params.require(:post).permit(:title, :description))
+  patch 'posts/:id', to: 'posts#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
